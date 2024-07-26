d82219fc54fc01d6cf053ed2403cf54922994786
lLuckyFauzi <luckyfauzi2004@gmail.com>
2024-07-26 09:06:38 +0700

    learning react behind the scenes and MillionJs

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import MillionLint from "@million/lint";
+
+export default defineConfig({
+  plugins: [MillionLint.vite(), react()],
+});
